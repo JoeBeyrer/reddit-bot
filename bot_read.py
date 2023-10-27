ba91d7ff7c2e66900437423abc1bf43af751a977
Joe Beyrer <joebeyrer@gmail.com>
2023-10-26 22:43:55 -0400

    Added subreddits + flair support

--- bot_read.py
+++ bot_read.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 import praw
+import pdb
+import re
+import os
 
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learnpython")
+learnpython = reddit.subreddit("learnpython")
+python = reddit.subreddit("python")
+learnprogramming = reddit.subreddit("learnprogramming")
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
+def getSubmissions(subreddit, flair=False):
+    for submission in subreddit.hot(limit=5):
+        print("Title: ", submission.title)
+        print("Text: ", submission.selftext)
+        print("Score: ", submission.score)
+        if flair:
+            print("Flair: ", submission.link_flair_text.strip().lower())
+        print("---------------------------------\n")
+
+getSubmissions(learnpython)
+getSubmissions(python, True)
+getSubmissions(learnprogramming)
